96ca468cb07cb54a052084f2aeedff5b3ffa9668
Santiago de Leon <sdeleon28@gmail.com>
2018-03-21 18:22:39 -0300

    Added redux store and hooks to modify it from the widget. Added comments everywhere.

--- android/app/src/main/java/com/androidwidgetpoc/WidgetProvider.java
+++ android/app/src/main/java/com/androidwidgetpoc/WidgetProvider.java
@@ -7,6 +7,11 @@ import android.util.Log;
 
 import com.facebook.react.HeadlessJsTaskService;
 
+/**
+ * Hello there, Android developer who already knows this stuff! You're going to hate how heavily commented this class
+ * is.
+ * Hello there, RN developer. You're going to love how heavily commented this class is.
+ */
 public class WidgetProvider extends AppWidgetProvider {
     private static final String WIDGET_TASK = "com.androidwidgetpoc.WIDGET_TASK";
 
@@ -14,7 +19,6 @@ public class WidgetProvider extends AppWidgetProvider {
     * When enabled on screen, let the BackgroundTaskBridge
     * manipulate it from javascript
     */
-
     @Override
     public void onEnabled(Context context) {
         Log.d("WIDGET_PROVIDER", "En onEnabled");
@@ -23,6 +27,11 @@ public class WidgetProvider extends AppWidgetProvider {
         HeadlessJsTaskService.acquireWakeLockNow(context);
     }
 
+    /**
+     * This is called when the user presses something on the widget.
+     *
+     * incomingIntent is the object that represents the touch event.
+     */
     @Override
     public void onReceive(final Context context, final Intent incomingIntent) {
         super.onReceive(context, incomingIntent);
@@ -31,13 +40,10 @@ public class WidgetProvider extends AppWidgetProvider {
             return;
         }
 
-        Intent silentStartIntent = new Intent(context, BackgroundTask.class);
-        context.startService(silentStartIntent);
-
-        /*
-        * Proxy bundle extras towards the service
-        * */
         Intent serviceIntent = new Intent(context, BackgroundTask.class);
+        // incomingIntent has what they call "extras" in the Android world.
+        // In this context, that's basically a bundle of data representing the touch event.
+        // Call serviceIntent.putExtras to send that data over to the js task.
         serviceIntent.putExtras(incomingIntent);
         context.startService(serviceIntent);
     }
